53738b9a7670a60b42af77f50076a48b4a939e5c
GitHubMADF1995 <1215514252@qq.com>
2021-03-13 23:25:29 +0800

    Netty实现群发给多个Client功能

--- src/main/java/com/madf/netty/HelloNetty.java
+++ src/main/java/com/madf/netty/HelloNetty.java
@@ -3,12 +3,20 @@ package com.madf.netty;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 public class HelloNetty {
+
+    //通道组，服务端接收到某个客户端消息时，将消息转发给其他每个客户端
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
     public static void main(String[] args) {
         new NettyServer(8888).serverStart();
     }
@@ -50,13 +58,32 @@ class NettyServer {
 
 class Handler extends ChannelInboundHandlerAdapter {
     @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//        super.channelActive(ctx);
+        HelloNetty.clients.add(ctx.channel());
+    }
+
+    @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 //        super.channelRead(ctx, msg);
         System.out.println("Server: channel read");
-        ByteBuf buf = (ByteBuf) msg;
+        ByteBuf buf = (ByteBuf) msg;//ByteBuf是直接访问操作系统的内容，因此它不在JVM垃圾回收机制之内
         System.out.println(buf.toString(CharsetUtil.UTF_8));
-        ctx.writeAndFlush(msg);
+        System.out.println("ByteBuf refCount: " + buf.refCnt());//ByteBuf引用的个数
+//        ctx.writeAndFlush(msg);//writeAndFlush自动释放掉ByteBuf所占用操作系统的内存
+        HelloNetty.clients.writeAndFlush(msg);//给多个客户端转发消息
         ctx.close();
+
+//        ByteBuf buf = null;
+//        System.out.println("Server: channel read");
+//        try {
+//            buf = (ByteBuf) msg;
+//            System.out.println(buf.toString(CharsetUtil.UTF_8));
+//            System.out.println("ByteBuf refCount: " + buf.refCnt());//ByteBuf引用的个数
+//        } finally {
+//            if (buf != null) ReferenceCountUtil.release(buf);
+//            System.out.println("ByteBuf refCount: " + buf.refCnt());
+//        }
     }
 
     @Override
